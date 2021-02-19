b5239acd968923427a9e3872e1889de52f72e369
Albenoo <albeniskerqeli5@gmail.com>
2021-02-19 18:36:34 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-let btn = document.querySelector(".btn");
+let btn = document.querySelector("#btn").addEventListener("click", GithubData);
+let row = document.querySelector(".row");
 // Grab
-btn.addEventListener("click", () => {
-  let inputval = document.querySelector("input").value;
+
+async function GithubData() {
+  let inputval = document.querySelector("#input").value;
 
   if (inputval == "") {
   } else {
-    fetch(`https://api.github.com/users/${inputval}`).then(response =>
-      response.json().then(data => {
-        console.log(data);
-        let box = document.querySelector(".box");
-        box.innerHTML = `
-<img src = ${data.avatar_url}>
-<h1>${data.login}</h1>
-<h2>${data.bio}</h2>
-
-<h1>Followers:${data.followers}, Following:${data.following}</h1>
+    const API = `https://api.github.com/users/${inputval}`;
+    const response = await fetch(API);
+    const data = await response.json();
+    console.log(data);
+    let box = document.querySelector(".profilecard");
+    box.style.display = "flex";
+    box.innerHTML = `
+ <div class="profile-img">
+  <img  src="${data.avatar_url}"/>
+   <h1 class="display-4"><a href="${data.html_url}">${data.name}</a></h1>
+ </div>
+ <div class="form-group">
+  <div class="text">
+    <h1 class="user-info">Followers</h1>
+     <h4 class="followers">${data.followers}</h4>
+   </div>
+   <div class="text">
+    <h1 class="user-info">Following</h1>
+                 <h4 class="following">${data.following}</h4>
+             </div>
+               <div class="text">
+                <h1 class="user-info">Repositories</h1>
+     <h4 class="total">${data.public_repos}</h4>
+    </div>
+ </div>
 `;
-      })
-    );
   }
-});
+}
